ed8e0fea25130193ef36419e76ffda871cd56924
Kenta Kodashima <kentakodashima@gmail.com>
2018-12-31 18:54:34 -0800

    Finish Transforming Observables part.

--- transforming-observables/20-transforming-operators-starter/RxSwiftPlayground/RxSwiftPlayground.playground/Contents.swift
+++ transforming-observables/20-transforming-operators-starter/RxSwiftPlayground/RxSwiftPlayground.playground/Contents.swift
@@ -1,7 +1,79 @@
 //: Please build the scheme 'RxSwiftPlayground' first
 import RxSwift
 
+example(of: "map") {
+  let disposeBag = DisposeBag()
+  
+  Observable.from(episodes)
+    .map {
+      var components = $0.components(separatedBy: " ")
+      let number = NSNumber(value: try! components[1].romanNumeralIntValue())
+      let numberString  = String(describing: number)
+      components[1] = numberString
+      return components.joined(separator: " ")
+    }
+    .subscribe(onNext: {
+      print($0)
+    })
+    .disposed(by: disposeBag)
+}
 
+// FlatMap keeps projecting changes from each Observable
+example(of: "flatMap") {
+  let disposeBag = DisposeBag()
+  
+  let ryan = Jedi(rank: BehaviorSubject(value: .youngling))
+  let charlotte = Jedi(rank: BehaviorSubject(value: .youngling))
+  
+  let student = PublishSubject<Jedi>()
+  
+  student
+    .flatMap {
+      $0.rank
+    }
+    .subscribe(onNext: {
+      print($0.rawValue)
+    })
+    .disposed(by: disposeBag)
+  
+  student.onNext(ryan)
+  
+  ryan.rank.onNext(.padawan)
+  
+  student.onNext(charlotte)
+  
+  ryan.rank.onNext(.jediKnight)
+  
+  charlotte.rank.onNext(.jediMaster)
+}
+
+example(of: "flatMapLatest") {
+  let disposeBag = DisposeBag()
+  
+  let ryan = Jedi(rank: BehaviorSubject(value: .youngling))
+  let charlotte = Jedi(rank: BehaviorSubject(value: .youngling))
+  
+  let student = PublishSubject<Jedi>()
+  
+  student
+    .flatMapLatest {
+      $0.rank
+    }
+    .subscribe(onNext: {
+      print($0.rawValue)
+    })
+    .disposed(by: disposeBag)
+  
+  student.onNext(ryan)
+  
+  ryan.rank.onNext(.padawan)
+  
+  student.onNext(charlotte)
+  
+  ryan.rank.onNext(.jediKnight)
+  
+  charlotte.rank.onNext(.jediMaster)
+}
 
 /*:
  Copyright (c) 2014-2018 Razeware LLC
